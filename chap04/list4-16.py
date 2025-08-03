6272c20b593de34cdddd1c1fcad24d2487baa80d
o2z3k7 <o.fumina5726@outlook.jp>
2025-08-03 21:59:10 +0900

    修正した

--- chap04/list4-16.py
+++ chap04/list4-16.py
@@ -1,2 +1,15 @@
 # 10~99の乱数をn個生成
-print("hogehoge")
+# print("hogehoge")
+
+import random
+
+n = int(input('乱数は何個：'))
+
+for _ in range(n):
+    r = random.randint(10, 99)
+    if (r == 13):
+        print('\n事情により中断します。')
+        break
+    print(r, end =' ')
+else:
+    print('\n乱数生成終了。')
