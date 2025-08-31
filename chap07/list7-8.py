5f0db016400037e1c5bf39b9ebe2c1bfb54fc3c8
o2z3k7 <o.fumina5726@outlook.jp>
2025-08-31 20:19:10 +0900

    20250831

---
+++ chap07/list7-8.py
@@ -0,0 +1,16 @@
+# 点数を読み込んで合計点・平均点を表示（その3）
+
+print('合計点と平均点を求めます。')
+number = int(input('学生の人数：'))
+
+tensu = [None] * number
+
+for i in range(number):
+    tensu[i] = int(input('{}番目の点数：'.format(i+1)))
+
+total=0
+for i, point in enumerate(tensu):
+    total += point
+
+print('合計は{}点です。'.format(total))
+print('平均は{}点です。'.format(total/number))
